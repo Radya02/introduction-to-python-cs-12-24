299461a3a5c4225d7f1908f724844ae3024be95d
Radyyaaaa <jishin02@mail.ru>
2025-01-15 10:43:37 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Докс")
+age = int(input("Какой твой вораст"))
+height = float(input("Какой у тебя рост в метрах?"))
+
+print(f"Тебе {age} лет и твой рост {height} метров")
+
+pasport = input("У тебя есть паспорт? да/нет")
+print(pasport, "молодец")
+
+names_parents = input("как зовут твоих родителей?")
+print(f"Твоих родителей зовут {names_parents}")
+
+print("Твои данные:")
+print(f"Твой возраст: {age}")
+print(f"Твой рост: {height}")
+print(f"Есть ли у тебя паспорт {pasport}")
+print(f"Имена твоих родителей {names_parents}")
