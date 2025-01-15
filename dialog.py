3dbbec1c0092c3ea62ea78ba9fa0643558ac7d14
Radyyaaaa <jishin02@mail.ru>
2025-01-15 12:19:35 +0600

    Create dialog.py

---
+++ dialog.py
@@ -0,0 +1,11 @@
+
+name = input("your name:")
+print("hello!", name)
+age = input("your age?")
+print("Wow! that's great")
+film = input("what's your favorite film?")
+print("Oh.. I like this film too!")
+book = input("favorite book?")
+print("You good person!")
+
+print(f"Okay! let's sum.","your name: {name} ", f"your age: {age}" , f"your favorite film {film}", f"your favorite book {book}")
